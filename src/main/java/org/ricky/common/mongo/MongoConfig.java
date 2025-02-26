0e5d0e22d4be687398208e9e59de414226bf60a3
Ricky <w_ziwei2004@163.com>
2025-02-26 15:35:06 +0800

    update properties

---
+++ src/main/java/org/ricky/common/mongo/MongoConfig.java
@@ -0,0 +1,17 @@
+package org.ricky.common.mongo;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Ricky
+ * @version 1.0
+ * @date 2025/2/26
+ * @className MongoConfig
+ * @desc
+ */
+@Configuration
+public class MongoConfig {
+
+
+
+}
