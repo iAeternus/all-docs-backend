d90f20c613f16f2c9177a2eb68f5b705e98971e2
Ricky <w_ziwei2004@163.com>
2025-02-26 15:01:02 +0800

    initial commit

---
+++ src/main/java/org/ricky/DocumentSharingSiteApplication.java
@@ -0,0 +1,13 @@
+package org.ricky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentSharingSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentSharingSiteApplication.class, args);
+    }
+
+}
