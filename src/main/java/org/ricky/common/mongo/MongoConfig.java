d6f6f9190a6b7cf7a88fc3e7ed8ca44e415964a7
Ricky <w_ziwei2004@163.com>
2025-02-26 15:38:21 +0800

    配置GridFSBucket

--- src/main/java/org/ricky/common/mongo/MongoConfig.java
+++ src/main/java/org/ricky/common/mongo/MongoConfig.java
@@ -1,5 +1,11 @@
 package org.ricky.common.mongo;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,6 +18,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MongoConfig {
 
+    /**
+     * 获取配置文件中数据库信息
+     */
+    @Value("${spring.data.mongodb.database}")
+    String db;
 
+    /**
+     * GridFSBucket用于打开下载流
+     *
+     * @param mongoClient MongoClient
+     * @return GridFSBucket
+     */
+    @Bean
+    public GridFSBucket getGridFsBucket(MongoClient mongoClient) {
+        MongoDatabase mongoDatabase = mongoClient.getDatabase(db);
+        return GridFSBuckets.create(mongoDatabase);
+    }
 
 }
